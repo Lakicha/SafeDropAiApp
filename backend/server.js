513e35fd0537a4a32c455fbad2185bb2336cfbae
Lakicha <128971476+Lakicha@users.noreply.github.com>
2025-12-17 02:54:47 +0300

    Update comment for serving frontend files

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve frontend
+// Serve frontend files
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('*', (req, res) => {
